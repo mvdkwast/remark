20b6c2b1cddd26e908de41b06065bfd2cf77a2e6
Martijn <martijn.vanderkwast@its-future.com>
2021-10-16 21:58:53 +0200

    wip: convert to es5

--- src/remark/models/slide.js
+++ src/remark/models/slide.js
@@ -1,136 +1,191 @@
-var converter = require('../converter');
-
 module.exports = Slide;
 
 function Slide (slideIndex, slideNumber, slide, template, options) {
-  var self = this;
+    var self = this;
 
-  self.properties = slide.properties || {};
-  self.links = slide.links || {};
-  self.content = slide.content || [];
-  self.notes = slide.notes || '';
+    self.properties = slide.properties || {};
+    self.links = slide.links || {};
+    self.content = slide.content || [];
+    self.notes = slide.notes || '';
 
-  self.getSlideIndex = function () { return slideIndex; };
-  self.getSlideNumber = function () { return slideNumber; };
+    self.getSlideIndex = function () { return slideIndex; };
+    self.getSlideNumber = function () { return slideNumber; };
 
-  if (template) {
-    inherit(self, template, options);
-  }
+    if (template) {
+        inherit(self, template, options);
+    }
 }
 
 function inherit (slide, template, options) {
-  inheritProperties(slide, template);
-  inheritContent(slide, template);
-  inheritNotes(slide, template, options);
+    inheritProperties(slide, template);
+    inheritContent(slide, template);
+    inheritNotes(slide, template, options);
 }
 
 function inheritProperties (slide, template) {
-  var property
-    , value
+    var property
+        , value
     ;
 
-  for (property in template.properties) {
-    if (!template.properties.hasOwnProperty(property) ||
-        ignoreProperty(property)) {
-      continue;
-    }
+    for (property in template.properties) {
+        if (!template.properties.hasOwnProperty(property) ||
+            ignoreProperty(property)) {
+            continue;
+        }
 
-    value = [template.properties[property]];
+        value = [template.properties[property]];
 
-    if (property === 'class' && slide.properties[property]) {
-      value.push(slide.properties[property]);
-    }
+        if (property === 'class' && slide.properties[property]) {
+            value.push(slide.properties[property]);
+        }
 
-    if (property === 'class' || slide.properties[property] === undefined) {
-      slide.properties[property] = value.join(', ');
+        if (property === 'class' || slide.properties[property] === undefined) {
+            slide.properties[property] = value.join(', ');
+        }
     }
-  }
 }
 
 function ignoreProperty (property) {
-  return property === 'name' ||
-    property === 'layout' ||
-    property === 'count';
+    return property === 'name' ||
+        property === 'layout' ||
+        property === 'count';
 }
 
 function inheritContent (slide, template) {
-  var expandedVariables;
+    var expandedVariables;
 
-  slide.properties.content = slide.content.slice();
-  deepCopyContent(slide, template.content);
+    slide.properties.content = slide.content.slice();
+    deepCopyContent(slide, template.content);
 
-  expandedVariables = slide.expandVariables(/* contentOnly: */ true);
+    expandedVariables = slide.expandVariables(/* contentOnly: */ true);
 
-  if (expandedVariables.content === undefined) {
-    slide.content = slide.content.concat(slide.properties.content);
-  }
+    if (expandedVariables.content === undefined) {
+        slide.content = slide.content.concat(slide.properties.content);
+    }
 
-  delete slide.properties.content;
+    delete slide.properties.content;
 }
 
 function deepCopyContent(target, content) {
-  var i;
+    var i;
 
-  target.content = [];
-  for (i = 0; i < content.length; ++i) {
-    if (typeof content[i] === 'string') {
-      target.content.push(content[i]);
+    target.content = [];
+    for (i = 0; i < content.length; ++i) {
+        if (typeof content[i] === 'string') {
+            target.content.push(content[i]);
+        }
+        else {
+            target.content.push({
+                block: content[i].block,
+                class: content[i].class,
+            });
+            deepCopyContent(target.content[target.content.length-1], content[i].content);
+        }
     }
-    else {
-      target.content.push({
-        block: content[i].block,
-        class: content[i].class,
-      });
-      deepCopyContent(target.content[target.content.length-1], content[i].content);
-    }
-  }
 }
 
 function inheritNotes (slide, template, options) {
-  if (template.notes && options.inheritPresenterNotes) {
-    slide.notes = template.notes + '\n\n' + slide.notes;
-  }
+    if (template.notes && options.inheritPresenterNotes) {
+        slide.notes = template.notes + '\n\n' + slide.notes;
+    }
 }
 
 Slide.prototype.expandVariables = function (contentOnly, content, expandResult) {
-  var properties = this.properties
-    , i
+    var properties = this.properties
+      , i
+      , expandedContent = []
+      , varRx = /(\\)?(\{\{([^\}\n]+)\}\})/
     ;
 
-  content = content !== undefined ? content : this.content;
-  expandResult = expandResult || {};
+    content = content !== undefined ? content : this.content;
+    expandResult = expandResult || {};
 
-  for (i = 0; i < content.length; ++i) {
-    if (typeof content[i] === 'string') {
-      content[i] = content[i].replace(/(\\)?(\{\{([^\}\n]+)\}\})/g, expand);
+    for (i = 0; i < content.length; ++i) {
+        if (typeof content[i] === 'string') {
+            var pieces = []
+              , start = 0
+              , match;
+
+            // expand variables, push in-between text + expands to pieces
+            while (match = varRx.exec(content[i])) {
+                var leadingText = content[i].substr(start, match.index);
+                if (leadingText !== '') {
+                    pieces.push(leadingText);
+                }
+
+                var expansion = expand(match[0], match[1], match[2], match[3]);
+                pieces.push(expansion);
+                start = match.index + match[0].length;
+            }
+
+            var trailingText = content[i].substr(start);
+            if (trailingText !== '') {
+                pieces.push(trailingText);
+            }
+
+            pieces = compact(pieces);
+            expandedContent = expandedContent.concat(pieces);
+        }
+        else {
+            this.expandVariables(contentOnly, content[i].content, expandResult);
+            expandedContent.push(content[i]);
+        }
     }
-    else {
-      this.expandVariables(contentOnly, content[i].content, expandResult);
+
+    // replace original content
+    content.length = 0;
+    content.push.apply(content, expandedContent);
+
+    function expand (match, escaped, unescapedMatch, property) {
+        var propertyName = property.trim()
+            , propertyValue
+        ;
+
+        if (escaped) {
+            return contentOnly ? match[0] : unescapedMatch;
+        }
+
+        if (contentOnly && propertyName !== 'content') {
+            return match;
+        }
+
+        propertyValue = properties[propertyName];
+        if (propertyValue instanceof Array && propertyValue.length === 1 && typeof propertyValue[0] === 'string') {
+            propertyValue = propertyValue[0];
+        }
+
+        if (propertyValue !== undefined) {
+            expandResult[propertyName] = propertyValue;
+            return propertyValue;
+        }
+
+        return propertyName === 'content' ? '' : unescapedMatch;
     }
-  }
 
-  function expand (match, escaped, unescapedMatch, property) {
-    var propertyName = property.trim()
-      , propertyValue
-      ;
+    function compact(pieces) {
+        if (pieces.length === 0) {
+            return [];
+        }
 
-    if (escaped) {
-      return contentOnly ? match[0] : unescapedMatch;
+        var compactedPieces = [pieces[0]],
+          i,
+          piece;
+
+        for (i=1; i<pieces.length; ++i) {
+            piece = pieces[i];
+            if (typeof piece === 'string' && typeof pieces[i-1] === 'string') {
+                compactedPieces[compactedPieces.length - 1] += piece;
+            }
+            else if (piece instanceof Array) {
+                compactedPieces = compactedPieces.concat(piece);
+            }
+            else {
+                compactedPieces.push(piece);
+            }
+        }
+
+        return compactedPieces;
     }
 
-    if (contentOnly && propertyName !== 'content') {
-      return match;
-    }
-
-    propertyValue = properties[propertyName];
-
-    if (propertyValue !== undefined) {
-      expandResult[propertyName] = propertyValue;
-      return propertyValue;
-    }
-
-    return propertyName === 'content' ? '' : unescapedMatch;
-  }
-
-  return expandResult;
+    return expandResult;
 };
